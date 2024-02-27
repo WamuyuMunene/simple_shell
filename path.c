9ff62d3b3fff3724ec21a0ac40088ba775bab2db
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-27 07:32:21 -0800

    looks for command in PATH

---
+++ path.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * check_path - looks for command in PATH
+ *
+ * @command: command
+ *
+ * Return: path or NULL
+ */
+
+char *check_path(char *command)
+{
+	char **path_array = NULL;
+	char *temp, *temp2, *path_cpy;
+	char *path = _getenv("PATH");
+	int i;
+
+	if (path == NULL || _strlen(path) == 0)
+		return (NULL);
+
+	path_cpy = malloc(sizeof(*path_cpy) * (_strlen(path) + 1));
+	_strcpy(path, path_cpy);
+	path_array = tokenizer(path_cpy, ":");
+
+	for (i = 0; path_array[i] != NULL; i++)
+	{
+		temp2 = _strcat(path_array[i], "/");
+		temp = _strcat(temp2, command);
+		if (access(temp, F_OK) == 0)
+		{
+			free(temp2);
+			free(path_array);
+			free(path_cpy);
+			return (temp);
+		}
+		free(temp);
+		free(temp2);
+	}
+
+	free(path_cpy);
+	free(path_array);
+	return (NULL);
+}
