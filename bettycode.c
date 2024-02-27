e439e57037995d4623ea9f77247b96109cc258b5
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-27 01:59:15 -0800

    code that passes betty checks

---
+++ bettycode.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - code that passes betty check
+ *
+ * @c: string
+ *
+ * Return: int
+ */
+
+int main(char *c)
+{
+	c = "Hello World";
+
+	printf("%s", c);
+
+	return (0);
+}
