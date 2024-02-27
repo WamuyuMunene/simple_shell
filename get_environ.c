5c2d24ac210eba0e6a3648bc99c2537cf4f68119
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-27 07:40:53 -0800

    fetch val of environ var

---
+++ get_environ.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _getenv - uses envirn var to fetch value
+ *
+ * @name: str name
+ *
+ * Return: NULL or val
+ */
+
+char *_getenv(char *name)
+{
+	char **my_environ;
+	char *pair_ptr;
+	char *name_cpy;
+
+	for (my_environ = environ; *my_environ != NULL; my_environ++)
+	{
+		for (pair_ptr = *my_environ, name_cpy = name;
+		     *pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
+		{
+			if (*pair_ptr == '=')
+				break;
+		}
+		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
+			return (pair_ptr + 1);
+	}
+	return (NULL);
+}
