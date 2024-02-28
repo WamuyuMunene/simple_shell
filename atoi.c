9d3947a8e8d661570066610fc593e8897deb090a
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-27 22:49:18 -0800

    convert str to int

---
+++ atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert str to int
+ *
+ * @s: str
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			return (-1);
+		else if ((*s < '0' || *s > '9') && *s != '\0')
+			return (-1);
+		else if (*s >= '0'  && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n);
+}
