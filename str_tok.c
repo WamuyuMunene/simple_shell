2b8b144f38486751f6252e6299ef46e68b6e6290
WamuyuMunene <wamuyumunene98@gmail.com>
2024-02-27 23:28:41 -0800

    split str to substr

---
+++ str_tok.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strtok_r - split str to smaller subsets
+ *
+ * @string: str
+ * @delim: measure
+ * @save_ptr: pointer
+ *
+ * Return: char *
+ */
+
+char *_strtok_r(char *string, char *delim, char **save_ptr)
+{
+	char *finish;
+
+	if (string == NULL)
+		string = *save_ptr;
+
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	string += _strspn(string, delim);
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	finish = string + _strcspn(string, delim);
+	if (*finish == '\0')
+	{
+		*save_ptr = finish;
+		return (string);
+	}
+
+	*finish = '\0';
+	*save_ptr = finish + 1;
+	return (string);
+}
